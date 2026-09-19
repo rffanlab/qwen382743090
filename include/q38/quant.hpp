59a01f7de1ac03fb28ce471493fc2adce2f0d0ff
rffanlab <admin@rffan.com>
2026-09-19 10:46:44 +0800

    feat: add include/q38/quant.hpp

---
+++ include/q38/quant.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+namespace q38 {
+
+inline constexpr std::size_t kQ4KValuesPerBlock = 256;
+inline constexpr std::size_t kQ4KBytesPerBlock = 144;
+
+float fp16_to_fp32(std::uint16_t bits) noexcept;
+void dequantize_q4_k_block_cpu(const std::byte* block, std::array<float, kQ4KValuesPerBlock>& out);
+
+} // namespace q38
