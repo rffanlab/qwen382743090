4ed04264f095186c34869ad8eb6fe43ba1125cc4
rffanlab <admin@rffan.com>
2026-09-19 10:50:56 +0800

    feat: add Q5_K block format constants

--- include/q38/quant.hpp
+++ include/q38/quant.hpp
@@ -8,8 +8,10 @@ namespace q38 {
 
 inline constexpr std::size_t kQ4KValuesPerBlock = 256;
 inline constexpr std::size_t kQ4KBytesPerBlock = 144;
+inline constexpr std::size_t kQ5KBytesPerBlock = 176;
 
 float fp16_to_fp32(std::uint16_t bits) noexcept;
 void dequantize_q4_k_block_cpu(const std::byte* block, std::array<float, kQ4KValuesPerBlock>& out);
+void dequantize_q5_k_block_cpu(const std::byte* block, std::array<float, kQ4KValuesPerBlock>& out);
 
 } // namespace q38
