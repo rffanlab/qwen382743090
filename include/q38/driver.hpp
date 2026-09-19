92cfbe282e197466ff6be87f63bf076ac9f39730
rffanlab <admin@rffan.com>
2026-09-19 10:56:50 +0800

    feat: expose fused Q5_K GEMV validation

--- include/q38/driver.hpp
+++ include/q38/driver.hpp
@@ -32,6 +32,7 @@ public:
     bool run_rmsnorm_smoke(std::string* error = nullptr, double* max_abs_error = nullptr, double* max_rel_error = nullptr);
     bool run_q4k_dequant_smoke(const std::byte* block, std::string* error = nullptr, double* max_abs_error = nullptr, double* max_rel_error = nullptr);
     bool run_q5k_dequant_smoke(const std::byte* block, std::string* error = nullptr, double* max_abs_error = nullptr, double* max_rel_error = nullptr);
+    bool run_q5k_gemv_smoke(const std::byte* matrix, std::uint32_t cols, std::uint32_t rows, std::string* error = nullptr, double* max_abs_error = nullptr, double* max_rel_error = nullptr, double* milliseconds = nullptr, double* bandwidth_gbps = nullptr);
 
 private:
     void* handle_{nullptr};
